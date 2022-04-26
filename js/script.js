833aa2bb46626c646ea2434e2bcb29625238a7cf
DevLBD <43916566+DevLBD@users.noreply.github.com>
2022-04-26 19:03:50 +0200

    Working on UI for posts.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const splash = document.getElementById("splash-screen");
 console.log((screen.width * 10) / 100);
 
 window.addEventListener("scroll", function() {
-    if (window.scrollY < 120){
+    if (window.scrollY < 120 && isPost != true){
         navBar.classList.remove("scrolled");
         title.classList.add("hidden");
     } else {
@@ -19,6 +19,10 @@ window.addEventListener("load", function(event) {
     this.setTimeout(function() {
         splash.classList.add("hidden");
     }, 805);
+
+    if (isPost == true) {
+        navBar.classList.add("scrolled");
+    }
 });
 
 window.switchPage = function(href) {
